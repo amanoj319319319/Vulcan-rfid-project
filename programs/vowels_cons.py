e613f1d93f16d39a34cf92b07e70280f8e9e0762
amanoj319319319 <a.manoj16@gmail.com>
2025-07-31 18:29:43 +0530

    vowels_cons file 1 is added

---
+++ programs/vowels_cons.py
@@ -0,0 +1,12 @@
+vowels_1=[]
+cons_1=[]
+name="southafrica"
+def vowels_cons(name):
+    for i in name:
+        if i in "aeiou":
+            vowels_1.append(i)
+        else:
+            cons_1.append(i)
+print(f"vowels are:", vowels_1)
+print(f"cons_are:", cons_1)
+
